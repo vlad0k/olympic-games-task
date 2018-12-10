4f59760867c31b63b7f3b3515c4fca32d39981aa
vladok089 <vladok.sha089@gmail.com>
2018-12-10 10:46:44 +0200

    mm

--- stats/medals_chart.js
+++ stats/medals_chart.js
@@ -1,24 +1,108 @@
+const sqlite3 = require('sqlite3').verbose();
+const EventEmmiter = require('events');
+
 function medals(params) {
-  medalParams = [];
+
+  var params = checkInput(params);
+
+  if (params == false) {
+    return;
+  }
+  getChart(params);
+}
+
+function checkInput(params) { // returns object with command line params
+
+  let medalParams = {};
+
   params.forEach((elem) => {
+    medalParams.medal = '0';
+
     switch(elem) {
       case 'summer':
-        medalParams[1] = elem;
+        medalParams.season = 'Summer';
+        break;
       case 'winter':
-        medalParams[1] = elem;
+        medalParams.season = 'Winter';
+        break;
       case 'gold':
-        medalParams[2] = 1;
+        medalParams.medal = 1;
+        break;
       case 'silver':
-        medalParams[2] = 2;
+        medalParams.medal = 2;
+        break;
       case 'bronze':
-        medalParams[2] = 3;
+        medalParams.medal = 3;
+        break;
     }
-    if (elem.length == 3){
-      medalParams[0] = elem.toUpperCase();
-    }
-  })
-  console.log(medalParams);
 
+    if (elem.length == 3){
+      medalParams.noc = elem.toUpperCase();
+    }
+  });
+
+  if (medalParams.season == undefined && medalParams.noc == undefined){
+    console.log('Season and NOC params are required');
+    return false;
+  }
+  else if (medalParams.season == undefined){
+    console.log('Season param is required');
+    return false;
+  }
+  else if (medalParams.noc == undefined){
+    console.log('NOC param is required');
+    return false;
+  }
+
+
+  return medalParams;
 }
+
+function getChart(params) {
+  const db = new sqlite3.Database('./db/olympic_history.db', sqlite3.OPEN_READONLY);
+  const dbEvents = new EventEmmiter();
+
+  db.all(`SELECT id FROM teams WHERE noc_name = (?)`, [params.noc], (err, rows) => {
+    params.teamID = rows[0].id;
+    dbEvents.emit('gotTeamId');
+  });
+
+  dbEvents.on('gotTeamId', () =>{
+    db.all('SELECT id FROM athletes WHERE team_id = (?)', [params.teamID], (err, rows) => {
+      params.athleteIDs = [];
+      for (i in rows){
+        params.athleteIDs.push(rows[i].id);
+      }
+      dbEvents.emit('gotAthlIDs');
+    });
+  });
+
+  dbEvents.on('gotAthlIDs', () => {
+    db.all(`SELECT id, year FROM games WHERE season = ?`, params.season, (err, rows) => {
+      params.games = rows;
+      dbEvents.emit('gotGames');
+    });
+  });
+
+  dbEvents.on('gotGames', () =>{
+    var checkGamesSQL = `game_id = (${params.games[0].id}`;
+    for (key in params.games){
+      checkGamesSQL += (key != 0) ? (' OR ' + params.games[key].id) : '';
+    }
+    checkGamesSQL += ')';
+    var checkMedalSQL = 'AND (medal'
+    if (params.medal != 0){
+      checkMedalSQL += ` = ${params.medal})`;
+    }
+    else {
+      checkMedalSQL += ' != 0)'
+    }
+    console.log(`SELECT athlete_id, game_id FROM results WHERE medal ${params.medal != 0 ? '= '+params.medal : '!= 0'}`);
+    db.all(`SELECT athlete_id, game_id FROM results WHERE medal ${params.medal != 0 ? '= '+params.medal : '!= 0'}`, (err, rows) => {
+      //check and count
+    })
+  });
+}
+
 
 module.exports = medals;
--- stats/stat_main.js
+++ stats/stat_main.js
@@ -1,3 +1,5 @@
+// console.clear();
+
 const medals = require('./medals_chart');
 const topTeams = require('./top-teams_chart');
 params = process.argv.splice(3);
@@ -9,5 +11,5 @@ else if (params[0] == "top-teams") {
   topTeams(params.splice(1));
 }
 else{
-  console.log('There no such chart');
+  console.log('There is no such chart');
 }
